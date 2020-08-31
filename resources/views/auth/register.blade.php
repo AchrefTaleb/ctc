2170ba3d8ad48746e1dda96c1e7bbad6ca823c2f
gitadmin <achref@viruscomm.ca>
2020-08-31 22:52:15 +0100

    mail

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -83,7 +83,9 @@
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <span class="small">
+                                    le mot de passe doit comporter 8 caractères minimum.
+                                </span>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
