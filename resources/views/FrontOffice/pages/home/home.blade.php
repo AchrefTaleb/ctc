bda112da44c75995d045a078de686d457886a43d
gitadmin <achref@viruscomm.ca>
2020-08-31 22:19:39 +0100

    mail

--- resources/views/FrontOffice/pages/home/home.blade.php
+++ resources/views/FrontOffice/pages/home/home.blade.php
@@ -75,7 +75,7 @@
                         <div class="widget-heading">
                             <div class="w-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>                            </div>
-                            <p class="w-value">{{ new \Carbon\Carbon($ends)->format('Y-m-d') }} <span class="float-right" style="font-weight: normal;"><a style="text-decoration: none;color: inherit" href="#">Fin de contract</a></span></p>
+                            <p class="w-value">{{ Carbon::parse($ends)->format('Y-m-d') }} <span class="float-right" style="font-weight: normal;"><a style="text-decoration: none;color: inherit" href="#">Fin de contract</a></span></p>
 
                         </div>
 
