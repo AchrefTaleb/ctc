65dc327c7cc69c0d8079fca43a0db042af4517ce
gitadmin <achref@viruscomm.ca>
2020-08-31 21:57:08 +0100

    mail

--- app/Http/Controllers/FrontOffice/HomeController.php
+++ app/Http/Controllers/FrontOffice/HomeController.php
@@ -14,15 +14,17 @@ class   HomeController extends Controller
 
     public function index()
     {
-        $nb_mails = Mail::all()->count();
-        $nb_mails_today = Mail::whereDate('created_at', Carbon::today())->get()->count();
+        $nb_mails = Mail::where('user_id',auth()->user()->id)->get()->count();
+        $nb_mails_today = Mail::where('user_id',auth()->user()->id)->whereDate('created_at', Carbon::today())->get()->count();
         $nb_requests = Req::where('user_id',auth()->user()->id)->where('status','approved')->get()->count();
+        $nb_notseen = Mail::where('user_id',auth()->user()->id)->where('open',false)->get()->count();
 
 
         return view('FrontOffice.pages.home.home',[
             'nb_mails' => $nb_mails,
             'nb_mails_today' => $nb_mails_today,
             'nb_requests' => $nb_requests,
+            'nb_notseen' => $nb_notseen,
         ]);
     }
 }
--- resources/views/FrontOffice/pages/home/home.blade.php
+++ resources/views/FrontOffice/pages/home/home.blade.php
@@ -58,6 +58,17 @@
 
                     </div>
                 </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 ">
+                    <div class="widget widget-one_hybrid widget-followers">
+                        <div class="widget-heading">
+                            <div class="w-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"></polygon></svg>                            </div>
+                            <p class="w-value">{{ $nb_notseen }} <span class="float-right" style="font-weight: normal;"><a href="{{ route('frontoffice.mail.list') }}">Courriers non lus</a></span></p>
+
+                        </div>
+
+                    </div>
+                </div>
 
 
             </div>
