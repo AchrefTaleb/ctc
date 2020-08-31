953b58f9496897c841a1858627dafdf122186660
gitadmin <achref@viruscomm.ca>
2020-08-31 22:30:09 +0100

    mail

--- resources/views/FrontOffice/pages/home/home.blade.php
+++ resources/views/FrontOffice/pages/home/home.blade.php
@@ -25,7 +25,7 @@
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 ">
                     <div class="widget widget-one_hybrid widget-followers">
                         <div class="widget-heading">
-                            <div >
+                            <div class="w-icon2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                             </div>
                             <p class="w-value">{{ $nb_mails_today }} <span class="float-right" style="font-weight: normal;">Nouveau courrier</span></p>
@@ -50,7 +50,7 @@
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 ">
                     <div class="widget widget-one_hybrid widget-followers">
                         <div class="widget-heading">
-                            <div >
+                            <div class="w-icon2" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"></polygon></svg>                            </div>
                             <p class="w-value">{{ $nb_requests }} <span class="float-right" style="font-weight: normal;"><a href="{{ route('frontoffice.mail.request.list') }}">Réexpidition à payer</a></span></p>
 
@@ -61,7 +61,7 @@
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 mt-3 ">
                     <div class="widget widget-one_hybrid widget-followers">
                         <div class="widget-heading">
-                            <div >
+                            <div class="w-icon2" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"></polygon></svg>
                             </div>
                             <p class="w-value">{{ $nb_notseen }} <span class="float-right" style="font-weight: normal;"><a href="{{ route('frontoffice.mail.list') }}">Courriers non lus</a></span></p>
