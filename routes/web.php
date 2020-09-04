3983ac7d9c142f4a0f75e5906274761203300e14
mayess ben el hadj <mayess.info@gmail.com>
2020-09-04 13:31:59 +0200

    emaildesign

--- routes/web.php
+++ routes/web.php
@@ -158,3 +158,6 @@ Route::prefix('frontoffice')->namespace('FrontOffice')->middleware(['auth','fron
 });
 
 Route::get('plans','BackOffice\PlanController@add_plans');
+Route::get('/emails', function(){
+    return new newUserMail();
+});
