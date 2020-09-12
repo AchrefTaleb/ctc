9f67a46fb54382330deacbbf3d8569ec69eb88de
gitadmin <achref@viruscomm.ca>
2020-09-12 14:18:48 +0100

    signupmail

--- resources/views/Emails/backend/signup.blade.php
+++ resources/views/Emails/backend/signup.blade.php
@@ -1,12 +1,19 @@
 @component('mail::message')
-Bonjour {{ $user->name.' '.$user->last_name }},
+Cher(e) client(e) {{ $user->name.' '.$user->last_name }},
 
-welcome text....
+Nous vous remercions de nous avoir fait confiance en choisissant le centre de transfert de courrier.
+Votre inscription à bien été prise en compte, elle sera validée dès réception de votre paiement bancaire.
+Vous pourrez alors vous connecter sur votre espace client via notre site avec le mot de passe que vous avez choisi et l’identifiant ci dessous:
+
+Votre identifiant : {{ $user->mail }}
 
 @component('mail::button', ['url' => config('app.APP_URL')])
 
 @endcomponent
 
-Merci,<br>
-{{ config('app.name') }}
+Vous pouvez également à tout moment modifier votre mot de passe en vous connectant dans votre espace client dans la rubrique: “paramètre - profil”.
+
+Merci de votre confiance.
+L’équipe transfert de courrier
+
 @endcomponent
--- resources/views/Emails/backend/subremainder.blade.php
+++ resources/views/Emails/backend/subremainder.blade.php
@@ -1,12 +1,14 @@
 @component('mail::message')
-Bonjour {{ $user->name.' '.$user->last_name }},
+Cher(e) client(e), {{ $user->name.' '.$user->last_name }},
 
-Remainder text.... //--> days left -> {{ $days }}
+Votre abonnement arrive à échéance dans {{ $days }} jours, il sera automatiquement renouvelé, veuillez verifier la validité de votre carte bancaire utilisé lors de l'inscription.
+Pour se désinscrire merci de contacter notres services client:
 
-@component('mail::button', ['url' => config('app.APP_URL')])
+                    Email : contact@transfertdecourrier.com
 
-@endcomponent
+                    Tel : +33 1 42 39 30 78
 
-Merci,<br>
-{{ config('app.name') }}
+Merci pour votre confiance,<br>
+L’équipe transfert de courrier.
+
 @endcomponent
