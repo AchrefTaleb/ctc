e364755001d37745243fb74e3746c92e51f16bc5
gitadmin <achref@viruscomm.ca>
2020-09-12 13:25:27 +0100

    opening

--- resources/views/FrontOffice/pages/mail/mail-list.blade.php
+++ resources/views/FrontOffice/pages/mail/mail-list.blade.php
@@ -48,7 +48,7 @@
                                     <td>{{ $mail->created_at->format('d-m-Y') ?? "-----" }}</td>
                                     <td><p>{{Str::limit($mail->description , 20, ' (...)')?? "-----" }}</p></td>
                                     <td class="text-center">
-                                        @if($opening == 'illimited' or $opening > 0)
+                                        @if($opening > 0)
                                             {{ $opening > 0 }}
                                         <ul class="table-controls">
 
