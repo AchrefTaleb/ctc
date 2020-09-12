630eda7da081efdb86786d21ee2c45bf223b408e
gitadmin <achref@viruscomm.ca>
2020-09-12 13:12:24 +0100

    opening

--- resources/views/FrontOffice/pages/mail/mail-list.blade.php
+++ resources/views/FrontOffice/pages/mail/mail-list.blade.php
@@ -1,7 +1,6 @@
 @extends('FrontOffice.layouts.layout')
 
 @section('content')
-    <div class="alert alert-warning mb-4" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x close" data-dismiss="alert"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button> <strong>Warning!</strong> Lorem Ipsum is simply dummy text of the printing. </div>
     <div class="row col-md-12 layout-top-spacing layout-spacing">
 
         <div class="col-md-12">
@@ -10,7 +9,9 @@
                     <div class="row">
                         <div class="col-xl-12 col-md-12 col-sm-12 col-12 inline">
                             <h4 class="float-left">Courrier</h4>
-                            <!-- <a href="{{ route('backoffice.mail.createform') }}" class="btn  mr-3 btn-warning float-right mt-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> Ajouter</a>-->
+                            <div class="float-right alert alert-warning mb-4" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x close" data-dismiss="alert"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button> <strong>Warning!</strong> Lorem Ipsum is simply dummy text of the printing. </div>
+
+                        <!-- <a href="{{ route('backoffice.mail.createform') }}" class="btn  mr-3 btn-warning float-right mt-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> Ajouter</a>-->
                         </div>
                         <div class="col-md-12">
                             <hr/>
