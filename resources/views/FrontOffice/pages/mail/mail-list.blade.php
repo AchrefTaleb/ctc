64e8ba1ca6f17ebe1bbfcae2e64c1001a440acba
gitadmin <achref@viruscomm.ca>
2020-09-13 18:31:09 +0100

    last mail

--- resources/views/FrontOffice/pages/mail/mail-list.blade.php
+++ resources/views/FrontOffice/pages/mail/mail-list.blade.php
@@ -42,7 +42,7 @@
                                     </td>
                                     <td>{{ $mail->code }}</td>
                                     <td>{{ $mail->from ?? "-----" }}</td>
-                                    <td>{{ $mail->client->id }}</span>{{ $mail->client->name.' '.$mail->client->last_name }}</td>
+                                    <td>{{ $mail->client->id }} - {{ $mail->client->name.' '.$mail->client->last_name }}</td>
                                     <td>{{ $mail->type ?? "-----" }}</td>
                                     <td>{{ $mail->category->name ?? "-----" }}</td>
                                     <td>{{ $mail->created_at->format('d-m-Y') ?? "-----" }}</td>
