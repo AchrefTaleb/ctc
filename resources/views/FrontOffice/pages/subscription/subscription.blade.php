2ecc4dcb5e251b4cd8118d535a1a10c0f31579c6
gitadmin <achref@viruscomm.ca>
2020-09-26 15:48:21 +0100

    commitment message

--- resources/views/FrontOffice/pages/subscription/subscription.blade.php
+++ resources/views/FrontOffice/pages/subscription/subscription.blade.php
@@ -17,6 +17,7 @@
                         <div class="acc-total-info" style="border: none">
                             <h5>{{ $subscription->plan->name }}</h5>
                             <p class="text-center">{{ $subscription->plan->description }}</p>
+                            <p class="text-center"> vous avez un engagemment de {{ $subscription->plan->commitment }} mois </p>
                             <p class="text-center"><span class="small ">{{ $subscription->plan->note }}</span></p>
                         </div>
                     </div>
