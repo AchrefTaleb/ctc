997e746a0f7e7fc859b40aa9979f8e622cd750c6
gitadmin <achref@viruscomm.ca>
2020-09-26 16:30:15 +0100

    promos

--- app/Http/Controllers/BackOffice/PromoController.php
+++ app/Http/Controllers/BackOffice/PromoController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class PromoController extends Controller
 {
-    public function index()
+    public function list()
     {
         $promo = promo::all();
         $promos = $promo->filter(function ($value) {
