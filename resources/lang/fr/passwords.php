68d34d715dd7a919d83b3ef37efc22229d2cbe8e
gitadmin <achref@viruscomm.ca>
2020-09-27 20:18:01 +0100

    resetpass

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,8 +13,8 @@ return [
     */
 
     'reset'     => 'Votre mot de passe a été réinitialisé !',
-    'sent'      => 'Nous vous avons envoyé par email le lien de réinitialisation du mot de passe !',
+    'sent'      => 'Nous vous avons envoyé par e-mail le lien de réinitialisation du mot de passe !',
     'throttled' => 'Veuillez patienter avant de réessayer.',
     'token'     => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
-    'user'      => "Aucun utilisateur n'a été trouvé avec cette adresse email.",
+    'user'      => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
 ];
