d8b9c5cd92ecec0aac7062258c03b0531a143b92
gitadmin <achref@viruscomm.ca>
2020-09-27 20:41:25 +0100

    resetpass

--- resources/views/BackOffice/globals/sidebar.blade.php
+++ resources/views/BackOffice/globals/sidebar.blade.php
@@ -100,8 +100,9 @@
 
                 </ul>
             </li>
+            @can('list-plan')
             <li class="menu">
-                <a href="#settings" data-toggle="collapse"  @if( (Route::currentRouteName() == 'backoffice.promo.list')||(Route::currentRouteName() == 'backoffice.promo.create') ) aria-expanded="true" data-active="true"   @else aria-expanded="false" data-active="false"  @endif class="dropdown-toggle">
+                <a href="#promo" data-toggle="collapse"  @if( (Route::currentRouteName() == 'backoffice.promo.list')||(Route::currentRouteName() == 'backoffice.promo.create') ) aria-expanded="true" data-active="true"   @else aria-expanded="false" data-active="false"  @endif class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg> <span>Code promotionnel</span>
                     </div>
@@ -109,7 +110,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
                     </div>
                 </a>
-                <ul class="collapse submenu list-unstyled  @if((Route::currentRouteName() == 'backoffice.staff.list')||(Route::currentRouteName() == 'backoffice.profile') ) show  @endif " id="settings" data-parent="#accordionExample">
+                <ul class="collapse submenu list-unstyled  @if((Route::currentRouteName() == 'backoffice.staff.list')||(Route::currentRouteName() == 'backoffice.profile') ) show  @endif " id="settings" data-parent="#promo">
                     @can('staff-list')
                     <li class="menu">
                         <a href="{{ route('backoffice.staff.list') }}">
@@ -126,7 +127,7 @@
 
                 </ul>
             </li>
-
+            @endcan
         </ul>
 
     </nav>
