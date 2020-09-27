924373a97b9fa2fa3c55c2e86cb105bc5c7ff1c8
gitadmin <achref@viruscomm.ca>
2020-09-27 20:07:15 +0100

    resetpass

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -13,9 +13,9 @@
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('adresse email') }}</label>
+                                <label for="email" class="col-md-12 col-form-label text-md-right">{{ __('adresse email') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                     @error('email')
@@ -27,7 +27,7 @@
                             </div>
 
                             <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
+                                <div class="col-md-12">
                                     <button type="submit" class="btn btn-primary">
                                         {{ __("Evoyer l'e-mail de récupération") }}
                                     </button>
