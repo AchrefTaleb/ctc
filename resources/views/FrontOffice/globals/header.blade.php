cc963743b877d03f0c1888838107d6a3d89fea1f
gitadmin <achref@viruscomm.ca>
2020-10-04 17:07:44 +0100

    show the contact ends date

--- resources/views/FrontOffice/globals/header.blade.php
+++ resources/views/FrontOffice/globals/header.blade.php
@@ -16,8 +16,7 @@
 
             <li class="nav-item dropdown user-profile-dropdown">
                 <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <img src="{{ asset('media/BackOffice/img/90x90.jpg') }}" alt="avatar">
-                </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg> {{ auth()->user()->name.' '.auth()->user()->last_name  }}                </a>
                 <div class="dropdown-menu position-absolute" aria-labelledby="userProfileDropdown">
                     <div class="">
                         <div class="dropdown-item">
