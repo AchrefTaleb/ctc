cc3b447e88e2d2a518be50b922d6c1c201fb1137
gitadmin <achref@viruscomm.ca>
2020-10-12 20:29:54 +0100

    plan

--- resources/views/BackOffice/pages/client/client-list.blade.php
+++ resources/views/BackOffice/pages/client/client-list.blade.php
@@ -48,7 +48,7 @@
                         <td>{{ $client->phone ?? "-----" }}</td>
                         <td>{{ $client->adresse ?? "-----" }}</td>
                         <td>@if($client->status) <span class="shadow-none badge badge-primary">Active</span> @else <span class="shadow-none badge badge-danger">Suspendu</span> @endif</td>
-                        <td> @if($client->subscription) {{ $client->subscription->created_at->format('m-y') ?? '' }} / {{ $client->subscription->commitment ?? '' }} @else  --- @endif </td>
+                        <td> @if($client->subscription) {{ $client->subscription->created_at->format('m-y') ?? '' }} / {{ $client->subscription->commitment ?? '' }}  / {{ $client->subscription->plan->name }}@else  --- @endif </td>
                        <td class="text-center">
                            <ul class="table-controls">
                                <li class="warning"><a href="{{ route('backoffice.client.updateform',['user' => $client->id]) }}" class="bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 p-1 br-6 mb-1"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a></li>
