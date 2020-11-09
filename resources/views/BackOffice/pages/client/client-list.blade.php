a400063dc1d65ee04afbe7755aed88e82c64836f
gitadmin <achref@viruscomm.ca>
2020-11-09 20:32:21 +0100

    contract

--- resources/views/BackOffice/pages/client/client-list.blade.php
+++ resources/views/BackOffice/pages/client/client-list.blade.php
@@ -30,6 +30,7 @@
                         <th>Adresse</th>
                         <th>Etat</th>
                         <th>Date / Engagement (mois)</th>
+                        <th>Etat de contrat</th>
                         <th class="no-content">Action</th>
                     </tr>
                     </thead>
@@ -49,6 +50,12 @@
                         <td>{{ $client->adresse ?? "-----" }}</td>
                         <td>@if($client->status) <span class="shadow-none badge badge-primary">Active</span> @else <span class="shadow-none badge badge-danger">Suspendu</span> @endif</td>
                         <td> @if($client->subscription) {{ $client->subscription->created_at->format('m-y') ?? '' }} / {{ $client->subscription->commitment ?? '' }}  / {{ $client->subscription->plan->name }}@else  --- @endif </td>
+                        <td> @if($client->contract) <span class="shadow-none badge badge-primary">contract signeé</span>   @else
+                                <form>
+                                    <input type="hidden" name="user" value="{{ $client->id }}">
+                                    <button type="submit" class="btn-info">Activer le contract</button>
+                                </form>
+                            @endif</td>
                        <td class="text-center">
                            <ul class="table-controls">
                                <li class="warning"><a href="{{ route('backoffice.client.updateform',['user' => $client->id]) }}" class="bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 p-1 br-6 mb-1"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a></li>
