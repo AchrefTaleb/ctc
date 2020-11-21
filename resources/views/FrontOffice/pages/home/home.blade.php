3613f15d33249c2ce13a8ddc231934a15ee2a95c
gitadmin <achref@viruscomm.ca>
2020-11-21 20:55:31 +0100

    remove contract from subscription

--- resources/views/FrontOffice/pages/home/home.blade.php
+++ resources/views/FrontOffice/pages/home/home.blade.php
@@ -22,7 +22,6 @@
     <div class="alert custom-alert-1 mb-4" role="alert">
         <div class="media bg-danger">
             <div class="alert-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-triangle"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12" y2="17"></line></svg>
             </div>
             <div class="media-body">
                 <div class="alert-text">
@@ -31,7 +30,7 @@
                         - Rendez-vous sur laposte.fr pour souscrire à votre contrat de réexpédition de courrier vers nos locaux.<br>
                         <strong> Étape 2 :</strong><br>
                          rendez-vous sur votre espace client C.T.C, pour finaliser et activer votre compte en joignant :<br>
-                        <ul>
+                        <ul class="ml-3">
                         <li>contrat signé</li>
                         <li>pièce d'identité</li>
                         <li>justificatif de domicile</li>
